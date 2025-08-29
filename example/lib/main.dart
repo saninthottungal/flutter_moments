e97c57a45a679bcd4560ec9afae1524be60c9aad
sanin564 <saninthottungalhere@gmail.com>
2025-08-29 20:50:53 +0530

    home page UI created

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,36 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return CupertinoApp(
+      home: CupertinoPageScaffold(
+        child: Center(
+          child: Builder(
+            builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    CupertinoPageRoute(
+                      builder: (context) => const MomentsPage(),
+                    ),
+                  );
+                },
+                child: const Text("Watch Moments 🎉"),
+              );
+            },
+          ),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MomentsPage extends StatelessWidget {
+  const MomentsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
+    return const CupertinoPageScaffold(
+      child: Center(
+        child: Text("Hello"),
       ),
-      body: const Center(child: Text("Hello Word")),
     );
   }
 }
--- lib/src/flutter_moments.dart
+++ lib/src/flutter_moments.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/widgets.dart';
 
+class Moments extends StatelessWidget {
+  const Moments({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
