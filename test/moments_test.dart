aa7276485567b2426e9ede9b9837917c30c19813
sanin564 <saninthottungalhere@gmail.com>
2025-08-29 16:32:52 +0530

    renaming to flutter_moments

--- test/moments_test.dart
+++ test/moments_test.dart
@@ -1,6 +1,5 @@
+import 'package:flutter_moments/flutter_moments.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:moments/moments.dart';
 
 void main() {
   test('adds one to input values', () {
